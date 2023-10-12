979fc0436a67362ab680bea905b4330a78c203e3
cammarze <135481707+cammarze@users.noreply.github.com>
2023-10-12 14:36:22 -0300

    agregue todas las funciones

--- funciones.py
+++ funciones.py
@@ -1,4 +1,18 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+def umbralizacion(img_arr):
+
+    umbral = input('Ingrese umbral: ')
+    while not umbral.isdecimal() or not 0 <= int(umbral) <= 255:
+        umbral = input('Error. Ingrese un numero entre 0 y 255: ')
+    umbral = float(umbral)
+    img_arr[img_arr >= umbral] = 255
+    img_arr[img_arr < umbral] = 0
+    plt.imshow(img_arr, cmap='gray')
+    plt.show()
+    return img_arr
+
 
 def identidad ():
     kernel_id = np.array([[0, 0, 0],
@@ -65,9 +79,15 @@ def motion_blur():
     kernel_motionblur = np.eye(11, dtype = int)[:, ::-1]
     return kernel_motionblur
 
-def kernel_personalizado (n):
-    personalizado_array = np.empty((n,n), dtype = float)
 
+def kernel_personalizado ():
+    n = input('Ingrese el tamaño del kernel: ')
+
+    while not n.isdecimal() or int(n) % 2 == 0:
+        n = input('Ingrese el tamaño del kernel: ')
+    n = int(n)
+
+    personalizado_array = np.empty((n,n), dtype = float)
     for f in range(1, n + 1):
         while True: 
             fila = (input(f"Ingrese los valores de la fila {f} separados por espacios: ")) 
@@ -82,8 +102,7 @@ def kernel_personalizado (n):
                     print('ERROR, los valores no son numeros reales')
     return personalizado_array
 
-def normalizacion (salida):
-    valor_minimo = np.min(salida)
-    valor_maximo = np.max(salida)
-    normalizado = ((salida - valor_minimo) / (valor_maximo - valor_minimo) * 255)
-    return normalizado
+
+operaciones = {'1':umbralizacion, '2':identidad, '3':negativo, '4':sobel_vertical, '5':sobel_horizontal,
+               '6':sharpen, '7':gaussian, '8':unsharpen, '9':box_blur, '10':lens_blur,
+               '11':motion_blur, '12':kernel_personalizado}
