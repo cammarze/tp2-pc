08fd2f158dceb9a5d632c60f5e558dbf6c2cb57e
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2023-10-14 18:32:39 -0300

    Normalizacion
    En el archivo de funciones modifique la normalizacion para que los valores de los pixeles esten entre 0 y 255

--- funciones.py
+++ funciones.py
@@ -1,19 +1,86 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def umbralizacion(img_arr):
-
+#Umbralizacion
+def umbralizacion(img_arr:list) -> list:
     umbral = input('Ingrese umbral: ')
-    while not umbral.isdecimal() or not 0 <= int(umbral) <= 255:
+    while not umbral.isdecimal() or not 0 <= float(umbral) <= 255:
         umbral = input('Error. Ingrese un numero entre 0 y 255: ')
     umbral = float(umbral)
+
     img_arr[img_arr >= umbral] = 255
     img_arr[img_arr < umbral] = 0
-    plt.imshow(img_arr, cmap='gray')
-    plt.show()
     return img_arr
 
+#Convolucion blanco y negro
+def convolucion (img, kernel):
+    img_arr = np.array(img, dtype=np.float32)
 
+    filas_img, cols_img = img.shape #Guardo las filas y columnas de la imagen
+    filas_kernel, cols_kernel = kernel.shape #Guardo las filas y columnas del kernel
+    conv_img = np.zeros(shape=(filas_img, cols_img)) #Creo un arreglo para la imagen modificada por el kernel
+
+    padding_fila = filas_kernel // 2
+    padding_col = cols_kernel // 2
+    img_pad = np.pad(img_arr, padding_fila, mode='edge') #Relleno los bordes de la imagen con los valores vecinos
+
+    for fila_img in range(filas_img): #Evito los bordes de la imagen
+        for col_img in range(cols_img):
+            resultado = 0
+            for fila_kernel in range(filas_kernel):
+                for col_kernel in range(cols_kernel):
+                    #Me muevo en la imagen con padding
+                    fila_actual = fila_img - padding_fila + fila_kernel
+                    col_actual = col_img - padding_col + col_kernel
+                    resultado += img_pad[fila_actual, col_actual] * kernel[fila_kernel, col_kernel]
+            conv_img[fila_img, col_img] = resultado
+            # aplica normalizacion a cada pixel de la imagenn
+    min_values = np.min(conv_img)
+    max_values = np.max(conv_img)
+    conv_img = (conv_img - min_values) / (max_values - min_values) #Pixeles quedan entre 0 y 1
+    conv_img *= 255
+    conv_img = conv_img.astype(np.uint8) #Convierte los pixeles con valores float a int (trunca)
+
+    return conv_img
+
+#Convolucion color
+def convolucion_rgb (img, kernel):
+    img_arr = np.array(img, dtype=np.float32)
+
+    filas_img, cols_img, canales = img_arr.shape 
+    filas_kernel, cols_kernel = kernel.shape 
+    conv_img = np.zeros(shape=(filas_img, cols_img, canales)) 
+
+    padding_fila = filas_kernel // 2
+    padding_col = cols_kernel // 2
+    img_pad = np.pad(img_arr, ((padding_fila, padding_fila), (padding_col, padding_col), (0, 0)), mode='edge') 
+    
+    for fila_img in range(filas_img):
+        for col_img in range(cols_img):
+            for canal in range(canales): #itera a traves de los canales de la imagen
+                resultado = 0
+                for fila_kernel in range(filas_kernel):
+                    for col_kernel in range(cols_kernel):
+                        fila_actual = fila_img - padding_fila + fila_kernel
+                        col_actual = col_img - padding_col + col_kernel
+                        resultado += img_pad[fila_actual, col_actual, canal] * kernel[fila_kernel, col_kernel]
+                conv_img[fila_img, col_img, canal] = resultado
+        # aplica normalizacion a cada pixel de la imagenn
+    min_values = np.min(conv_img, axis=(0, 1)) # con axis te devuelve el valor max y min de cada canal
+    max_values = np.max(conv_img, axis=(0, 1))
+    conv_img = (conv_img - min_values) / (max_values - min_values) #Pixeles quedan entre 0 y 1
+    conv_img *= 255
+    conv_img = conv_img.astype(np.uint8) #Convierte los pixeles con valores float a int (trunca)
+    return conv_img
+
+#Normalizacion
+def normalizacion (salida):
+    valor_minimo = np.min(salida)
+    valor_maximo = np.max(salida)
+    normalizado = ((salida - valor_minimo) / (valor_maximo - valor_minimo) * 255)
+    return normalizado
+
+#Kernels
 def identidad ():
     kernel_id = np.array([[0, 0, 0],
                          [0, 1, 0],
@@ -79,12 +146,10 @@ def motion_blur():
     kernel_motionblur = np.eye(11, dtype = int)[:, ::-1]
     return kernel_motionblur
 
-
 def kernel_personalizado ():
-    n = input('Ingrese el tamaño del kernel: ')
-
-    while not n.isdecimal() or int(n) % 2 == 0:
-        n = input('Ingrese el tamaño del kernel (debe ser un numero e impar): ')
+    n = input('Ingrese un numero: ')
+    while not n.isdecimal():
+        n = input('Error. Ingrese un numero: ')
     n = int(n)
 
     personalizado_array = np.empty((n,n), dtype = float)
@@ -102,7 +167,7 @@ def kernel_personalizado ():
                     print('ERROR, los valores no son numeros reales')
     return personalizado_array
 
-
+#Diccionario de operaciones
 operaciones = {'1':umbralizacion, '2':identidad, '3':negativo, '4':sobel_vertical, '5':sobel_horizontal,
                '6':sharpen, '7':gaussian, '8':unsharpen, '9':box_blur, '10':lens_blur,
                '11':motion_blur, '12':kernel_personalizado}
--- programa.py
+++ programa.py
@@ -2,51 +2,61 @@ from PIL import Image #Para leer imagenes
 import numpy as np #Para trabajar con arreglos (matrices)
 import matplotlib.pyplot as plt  #Para ver la imagen
 import funciones as f
-import convolucion as conv
 
-def open_image (): #funcion para abrir la imagen
+# def open_image (): #funcion para abrir la imagen
+#     try:
+#         img_ing = input('Ingrese el nombre del archivo de la imagen: ')
+#         imagen = Image.open(img_ing)
+#             # imagen = Image.open('test_images//' + img_ing)
+#         img_arr = np.array(imagen)
+#         return img_arr
+
+#     except FileNotFoundError:
+#         print("Imagen no encontrada!")
+#         exit()
+def main():
     try:
         img_ing = input('Ingrese el nombre del archivo de la imagen: ')
-        imagen = Image.open(img_ing)
+        imagen = Image.open('test_images//' + img_ing)
             # imagen = Image.open('test_images//' + img_ing)
         img_arr = np.array(imagen)
-        return img_arr
+
+            #img_arr = np.array(open_image())
+        plt.imshow(img_arr, cmap='gray')
+        plt.show()
+
+        print('OPERACIONES')
+        print('-----------')
+        for i, operacion in enumerate(f.operaciones.values(), 1): 
+            print(f'{i}. {operacion.__name__}') #imprimir los tipos de kernel
+
+        opcion = input('Ingrese la operacion: ') #si la opcion ingresada esta fuera del rango de opciones, muestra mensaje de error
+        if 1 > int(opcion) or int(opcion) > 12:
+            print("Operación inválida!")
+            exit()
+
+        if opcion == '1': 
+            img_arr = f.umbralizacion(img_arr) 
+
+        else:
+            kernel = f.operaciones[opcion]()
+
+            if len(img_arr.shape) == 2:  #verifica si la imagen es en escala de grises (2D)
+
+                convolucion_img = f.convolucion(img_arr, kernel)
+                plt.imshow(convolucion_img, cmap ='gray')
+                plt.show()
+                
+            else: # para las imagenes RGB
+                img_conv_rgb = f.convolucion_rgb(img_arr, kernel)
+                plt.imshow(img_conv_rgb)
+                plt.show()
+            
+        print('Operacion realizada con exito!')
 
     except FileNotFoundError:
         print("Imagen no encontrada!")
         exit()
-    
+
 if __name__ == "__main__":
-    img_arr = np.array(open_image())
-    plt.imshow(img_arr, cmap='gray')
-    plt.show()
-
-    print('OPERACIONES')
-    print('-----------')
-    for i, operacion in enumerate(f.operaciones.values(), 1): 
-        print(f'{i}. {operacion.__name__}') #imprimir los tipos de kernel
-
-    opcion = input('Ingrese la operacion: ') #si la opcion ingresada esta fuera del rango de opciones, muestra mensaje de error
-    if 1 > int(opcion) or int(opcion) > 12:
-        print("Operación inválida!")
-        exit()
-
-    if opcion == '1': 
-        img_arr = f.umbralizacion(img_arr) 
-
-    else:
-        kernel = f.operaciones[opcion]()
-
-        if len(img_arr.shape) == 2:  #verifica si la imagen es en escala de grises (2D)
-
-            convolucion_img = conv.convolucion(img_arr, kernel)
-            plt.imshow(convolucion_img, cmap ='gray')
-            plt.show()
-
-        else: # para las imagenes RGB
-            img_conv_rgb = conv.convolucion_rgb(img_arr, kernel)
-            plt.imshow(img_conv_rgb, cmap = 'gray')
-            plt.show()
-        
-    print('Operacion realizada con exito!')
-
+    main()
