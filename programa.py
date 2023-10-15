31c5a92db5506118e21c939449a2b98a22db1ba7
cammarze <135481707+cammarze@users.noreply.github.com>
2023-10-14 21:59:08 -0300

    docstringg

--- programa.py
+++ programa.py
@@ -3,25 +3,30 @@ import numpy as np #Para trabajar con arreglos (matrices)
 import matplotlib.pyplot as plt  #Para ver la imagen
 import funciones as f
 
-# def open_image (): #funcion para abrir la imagen
-#     try:
-#         img_ing = input('Ingrese el nombre del archivo de la imagen: ')
-#         imagen = Image.open(img_ing)
-#             # imagen = Image.open('test_images//' + img_ing)
-#         img_arr = np.array(imagen)
-#         return img_arr
-
-#     except FileNotFoundError:
-#         print("Imagen no encontrada!")
-#         exit()
 def main():
+"""
+Programa principal para llevar a cabo el procesamiento de las imagenes. Permite al usuario cargar la imagen (posteriormente visualizarla),
+y seleccionar una operación (donde se aplica la umbralizacion o la convolucion con distintos tipos de kernels). 
+
+INteracciones con el usuario:
+-----------------------------
+- El programa le solicita el nombre del archivo.
+-  La imagen se carga y posteriormente se muestra una lista con las distintas operaciones. 
+- El usuario debe ingresar el numero correspondiente de la operacion.
+- Para "Kernel personalizado" -- el usuario tiene la posibilidad de ingresar el tamaño y los valores del kernel. 
+- Despues de la selección, se solicita al usuario ingresar un nombre para el archivo de salida (formato PNG).
+- Se aplica la operacion a la imagen igresada, luego muestra el resultado. 
+
+Si el nombre del archivo no existe o el numero de la operacion ingresada no se ecuentra en la lista, se muestra 
+un mensaje de error y el programa termina. 
+
+La ejecución del programa comienza al llamar a main().
+"""
     try:
         img_ing = input('Ingrese el nombre del archivo de la imagen: ')
         imagen = Image.open('test_images//' + img_ing)
-            # imagen = Image.open('test_images//' + img_ing)
         img_arr = np.array(imagen)
-
-            #img_arr = np.array(open_image())
+        
         plt.imshow(img_arr, cmap='gray')
         plt.show()
 
@@ -36,12 +41,13 @@ def main():
             exit()
 
         if opcion == '1': 
+            # aplica umbralizacion a la imagen utilizando la función umbralizacion del modulo 'f'
             img_arr = f.umbralizacion(img_arr) 
 
         else:
             kernel = f.operaciones[opcion]()
 
-            if len(img_arr.shape) == 2:  #verifica si la imagen es en escala de grises (2D)
+            if len(img_arr.shape) == 2:  #verifica si la imagen es en escala de grises
 
                 convolucion_img = f.convolucion(img_arr, kernel)
                 plt.imshow(convolucion_img, cmap ='gray')
